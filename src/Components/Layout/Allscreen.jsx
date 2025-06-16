458c53c9bc6c62aba8554109a9d5e0879fa84cba
Your Name <fawzyfamoz@gmail.com>
2025-06-16 16:12:43 +0100

    initial commit

---
+++ src/Components/Layout/Allscreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../Auth/LogIn'
+import SignUp from '../Auth/SignUp'
+
+const Allscreen = () => {
+  return (
+    <div>
+        <Login/>
+        <SignUp/>
+    </div>
+  )
+}
+
+export default Allscreen
