b33a6bc2aff7b2c7a3a126df5be3b3472044bbad
Dee33Kode <iheyinnadaniel91@gmail.com>
2025-06-16 14:26:58 +0100

    Updated

---
+++ src/Components/Layout/Allscreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SubAdmin from '../Dashboards/SubAdmin'
+import SuperAdmin from '../Dashboards/SuperAdmin'
+
+
+const Allscreen = () => {
+  return (
+    <div>
+        <SubAdmin/>
+        <SuperAdmin/>
+    </div>
+  )
+}
+
+export default Allscreen
