98d021217f9bb38e5a786a3d8587f30240de63ad
Dee33Kode <iheyinnadaniel91@gmail.com>
2025-06-20 16:10:35 +0100

    update

--- src/Components/Layout/Allscreen.jsx
+++ src/Components/Layout/Allscreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AllRoutes from '../Routes/AllRoutes'
+import AllRoutes from '../../Components/Pages/Routes/AllRoutes'
 const Allscreen = () => {
   return (
     <div>
